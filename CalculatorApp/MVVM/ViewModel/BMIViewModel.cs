0f868aa63b247b5bdbd1772d0906cf4a947ee68f
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2024-05-12 11:18:12 +0100

    Adding option to choose if user is a male or a woman.

--- CalculatorApp/MVVM/ViewModel/BMIViewModel.cs
+++ CalculatorApp/MVVM/ViewModel/BMIViewModel.cs
@@ -1,14 +1,52 @@
 ﻿using CalculatorApp.MVVM.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CalculatorApp.MVVM.ViewModel
 {
-    public class BMIViewModel
+    public class BMIViewModel : INotifyPropertyChanged
     {
+        private Color SELECTED = Colors.Gray;
+        private Color UNSELECTED = Colors.White;
+
+        private Color _genderMale;
+        private Color _genderFamale;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public Color GenderFamale
+        {
+            get => _genderFamale;
+            set
+            {
+                _genderFamale = value;
+                _genderMale = UNSELECTED;
+
+                OnPropertyChanged(nameof(GenderFamale));
+                OnPropertyChanged(nameof(GenderMale));
+            }
+        }
+
+        public Color GenderMale
+        {
+            get => _genderMale;
+            set
+            {
+                _genderFamale = UNSELECTED;
+                _genderMale = value;
+
+                OnPropertyChanged(nameof(GenderFamale));
+                OnPropertyChanged(nameof(GenderMale));
+            }            
+        }
+
+        public ICommand SelectedGenderAsMale {  get; }
+        public ICommand SelectedGenderAsFamale { get; }
         public BMI BMI { get; set; }
 
         public BMIViewModel()
@@ -16,6 +54,24 @@ namespace CalculatorApp.MVVM.ViewModel
             BMI = new BMI();
             BMI.Height = 180;
             BMI.Weight = 73;
+            GenderMale = SELECTED;           
+            SelectedGenderAsFamale = new Command(ExecuteSelectGenderAsFamale);
+            SelectedGenderAsMale = new Command(ExecuteSelectGenderAsMale);
+        }
+
+        private void ExecuteSelectGenderAsMale()
+        {
+            GenderMale = SELECTED;
+        }
+
+        private void ExecuteSelectGenderAsFamale()
+        {
+            GenderFamale = SELECTED;
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
